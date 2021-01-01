f03f6d2dfed45f28bd65d5e8c08905473d02ff8c
namdo1012 <namdo10121999@gmail.com>
2021-01-01 22:19:27 +0700

    Ghép base Huy, tạo giao diện và liên kết các màn

--- src/gui.c
+++ src/gui.c
@@ -17,14 +17,16 @@ int gui(int serverfd) {
 
   // Get window
   window = GTK_WIDGET(gtk_builder_get_object(builder, WINDOW_MAIN));
-  widgets->w_container_menu_log =
-      GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_MENU_LOG));
-
-  // Get stack
-  widgets->w_stack_container =
-      GTK_STACK(gtk_builder_get_object(builder, STACK_CONTAINER));
-  widgets->w_stack_menu =
-      GTK_STACK(gtk_builder_get_object(builder, STACK_MENU));
+  widgets->w_container_feature = GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_FEATURE));
+  widgets->w_container_listroom = GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_LISTROOM));
+  widgets->w_container_changepas = GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_CHANGEPAS));
+  widgets->w_container_menu = GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_MENU));
+  widgets->w_container_menu_reg = GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_MENU_REG));
+  widgets->w_container_menu_log = GTK_WIDGET(gtk_builder_get_object(builder, CONTAINER_MENU_LOG));
+  
+  //Get stack
+  widgets->w_stack_home = GTK_STACK(gtk_builder_get_object(builder, STACK_HOME));
+  widgets->w_stack_menu = GTK_STACK(gtk_builder_get_object(builder, STACK_MENU));
 
   // Get entry
   widgets->w_entry_menu_log_user =
@@ -46,20 +48,6 @@ int gui(int serverfd) {
   return 0;
 }
 
-void on_btn_menu_login_clicked(GtkButton *button, app_widgets *app_wdgts) {
-  char username[MAX], password[MAX];
-  char request[MAX];
-
-  strcpy(username, gtk_entry_get_text(app_wdgts->w_entry_menu_log_user));
-  strcpy(password, gtk_entry_get_text(app_wdgts->w_entry_menu_log_pas));
-  sprintf(request, "signIn-%s-%s", username, password);
-  sendData(app_wdgts->serverfd, request);
-
-  return;
-}
-
-void on_window_main_destroy() { gtk_main_quit(); }
-
 void *recv_handler(void *app_widget) {
   int serverfd;
   char response[MAX];
@@ -77,4 +65,63 @@ void *recv_handler(void *app_widget) {
   }
 
   close(serverfd);
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
+
+void on_btn_listroom_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_listroom);
+    return;
+}
+
+void on_btn_backhome_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_feature);
+    return;
+}
+
+void on_btn_reg_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_feature);
+    return;
+}
+
+void on_btn_log_clicked(GtkButton *button, app_widgets *app_wdgts) {
+  char username[MAX], password[MAX];
+  char request[MAX];
+
+  strcpy(username, gtk_entry_get_text(app_wdgts->w_entry_menu_log_user));
+  strcpy(password, gtk_entry_get_text(app_wdgts->w_entry_menu_log_pas));
+  sprintf(request, "signIn-%s-%s", username, password);
+  sendData(app_wdgts->serverfd, request);
+  gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_feature);
+  return;
+}
+// void on_btn_log_clicked(GtkButton *button, app_widgets *app_wdgts){
+//     gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_feature);
+//     return;
+// }
+
+void on_btn_logout_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_menu);
+    gtk_stack_set_visible_child(app_wdgts->w_stack_menu, app_wdgts->w_container_menu_log);
+    return;
+}
+
+void on_btn_viewchangepas_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_changepas);
+    return;
+}
+
+void on_btn_changepas_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_menu);
+    gtk_stack_set_visible_child(app_wdgts->w_stack_menu, app_wdgts->w_container_menu_log);
+    return;
+}
+
+void on_btn_changepas_back_clicked(GtkButton *button, app_widgets *app_wdgts){
+    gtk_stack_set_visible_child(app_wdgts->w_stack_home, app_wdgts->w_container_feature);
+    return;
 }
--- src/gui.h
+++ src/gui.h
@@ -6,20 +6,27 @@
 #include "constant.h"
 #include "message.h"
 
-#define GLADE_FILE_NAME "glade/clientGUI.glade"
+// #define GLADE_FILE_NAME "glade/clientGUI.glade"
+#define GLADE_FILE_NAME "glade/nam.glade"
 #define GLADE_CSS "glade/style.css"
 
 // ID Window
 #define WINDOW_MAIN "window_main"
 
 // ID Stack
-#define STACK_CONTAINER "stack_container"
+// #define STACK_CONTAINER "stack_container"
+// #define STACK_MENU "stack_menu"
+#define STACK_HOME "stack_home"
 #define STACK_MENU "stack_menu"
 
 // ID Container
 #define CONTAINER_MENU "container_menu"
+#define CONTAINER_FEATURE "container_feature"
+#define CONTAINER_LISTROOM "container_listroom"
+#define CONTAINER_CHANGEPAS "container_changepas"
 #define CONTAINER_MENU_LOG "container_menu_log"
-#define NUMBER_OF_CONTAINER 4
+#define CONTAINER_MENU_REG "container_menu_reg"
+#define NUMBER_OF_CONTAINER 6
 
 // ID Entry
 #define ENTRY_MENU_LOG_USER "entry_menu_log_user"
@@ -33,9 +40,9 @@ typedef struct {
   int serverfd;
   Message *msg;
 
-  GtkEntry *w_entry_menu_log_user, *w_entry_menu_log_pas;
-  GtkStack *w_stack_container, *w_stack_menu;
-  GtkWidget *w_container_menu_log;
+  GtkEntry  *w_entry_menu_log_user, *w_entry_menu_log_pas;
+  GtkStack  *w_stack_home, *w_stack_menu;
+  GtkWidget *w_container_feature, *w_container_listroom, *w_container_menu, *w_container_menu_reg, *w_container_menu_log, *w_container_changepas;
 } app_widgets;
 
 GtkBuilder *builder;
